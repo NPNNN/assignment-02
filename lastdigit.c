c11299274c434010c3a9cc3454b8d291940e1349
NARENDAR KUMAR NISHAD <118038477+NPNNN@users.noreply.github.com>
2022-12-25 21:23:03 +0530

    Add files via upload

---
+++ lastdigit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,r;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    r=n/10;
+    printf("The number whithout last digit is %d",r);
+    return 0;
+
+}
